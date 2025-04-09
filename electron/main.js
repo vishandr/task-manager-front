cbcfff9f1951f811fd0f3dbb7c6dec2fd5819319
vishandr <101496759+vishandr@users.noreply.github.com>
2025-04-09 18:14:07 +0300

    Initial commit

---
+++ electron/main.js
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // если нужен preload
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL('http://localhost:3000'); // Vite dev server
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
